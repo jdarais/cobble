7fe973afd5ab4a491cdf664277efdf5c78bd2a94
Jeremiah Darais <jdarais@gmail.com>
2024-08-28 21:42:10 -0700

    cmd tool prints command

--- src/load.rs
+++ src/load.rs
@@ -170,6 +170,9 @@ pub fn extract_project_defs(lua: &mlua::Lua) -> mlua::Result<HashMap<String, Pro
         function (c)
             local cmd = require("cmd")
             local tblext = require("tblext")
+            
+            c.println("cmd: "..table.concat(c.args, " "))
+
             local result = cmd(tblext.extend({ cwd = c.project.dir, out = c.print, err = c.eprint }, c.args))
 
             if result.status ~= 0 then
